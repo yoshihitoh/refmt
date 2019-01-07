e3c797a823737c47bac083fea4bcdad3a266525b
yoshihitoh <yoshihito.arih@gmail.com>
2019-01-07 22:17:26 +0900

    Fix typo

--- src/assets.rs
+++ src/assets.rs
@@ -5,21 +5,21 @@ use syntect::highlighting::ThemeSet;
 use syntect::parsing::{SyntaxSet, SyntaxSetBuilder};
 
 struct AssetBuilder<'a> {
-    asset_dir: &'a Path,
+    assets_dir: &'a Path,
 }
 
 impl<'a> AssetBuilder<'a> {
     fn build_syntaxes(&self) -> Result<SyntaxSet, syntect::LoadingError> {
         let mut syntax_set_builder = SyntaxSetBuilder::new();
         syntax_set_builder.add_plain_text_syntax();
-        syntax_set_builder.add_from_folder(self.asset_dir.join("syntaxes"), true)?;
+        syntax_set_builder.add_from_folder(self.assets_dir.join("syntaxes"), true)?;
 
         Ok(syntax_set_builder.build())
     }
 
     fn build_themes(&self) -> Result<ThemeSet, syntect::LoadingError> {
         let mut theme_set = ThemeSet::default();
-        theme_set.add_from_folder(self.asset_dir.join("themes"))?;
+        theme_set.add_from_folder(self.assets_dir.join("themes"))?;
 
         Ok(theme_set)
     }
@@ -57,18 +57,18 @@ impl HighlightAssets {
         })
     }
 
-    pub fn build_from_files(asset_dir: &Path) -> Result<HighlightAssets, syntect::LoadingError> {
-        let builder = AssetBuilder { asset_dir };
+    pub fn build_from_files(assets_dir: &Path) -> Result<HighlightAssets, syntect::LoadingError> {
+        let builder = AssetBuilder { assets_dir };
         Ok(HighlightAssets {
             syntax_set: builder.build_syntaxes()?,
             theme_set: builder.build_themes()?,
         })
     }
 
-    pub fn save(&self, asset_dir: &Path) -> Result<(), bincode::Error> {
+    pub fn save(&self, assets_dir: &Path) -> Result<(), bincode::Error> {
         // TODO: Remove bincode from dependencies. It is only used for this error handling.
-        dump_to_file(&self.syntax_set, asset_dir.join("syntaxes.bin"))?;
-        dump_to_file(&self.theme_set, asset_dir.join("themes.bin"))?;
+        dump_to_file(&self.syntax_set, assets_dir.join("syntaxes.bin"))?;
+        dump_to_file(&self.theme_set, assets_dir.join("themes.bin"))?;
 
         Ok(())
     }
--- src/bin/generate_assets.rs
+++ src/bin/generate_assets.rs
@@ -5,7 +5,7 @@ use reser_core::assets::HighlightAssets;
 
 #[derive(Debug)]
 struct ProgramOption {
-    assert_dir: String,
+    assets_dir: String,
 }
 
 #[derive(Debug)]
@@ -29,23 +29,23 @@ impl From<bincode::Error> for GenerateError {
 fn parse_args() -> ProgramOption {
     let m = App::new("generate_assets")
         .arg(
-            Arg::with_name("ASSET_DIR")
-                .long("asset-dir")
-                .help("Set path to asset directory")
+            Arg::with_name("ASSETS_DIR")
+                .long("assets-dir")
+                .help("Set path to assets directory")
                 .takes_value(true)
-                .value_name("ASSET_DIR")
+                .value_name("ASSETS_DIR")
                 .required(true),
         )
         .get_matches();
 
     ProgramOption {
-        assert_dir: m.value_of("ASSET_DIR").unwrap().to_string(),
+        assets_dir: m.value_of("ASSETS_DIR").unwrap().to_string(),
     }
 }
 
 fn main() -> Result<(), GenerateError> {
     let options = parse_args();
-    let assets = HighlightAssets::build_from_files(Path::new(&options.assert_dir))?;
-    assets.save(Path::new(&options.assert_dir))?;
+    let assets = HighlightAssets::build_from_files(Path::new(&options.assets_dir))?;
+    assets.save(Path::new(&options.assets_dir))?;
     Ok(())
 }
