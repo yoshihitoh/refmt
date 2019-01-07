781042ae0a2137d0b5a27d1a65072b06bc57143a
yoshihitoh <yoshihito.arih@gmail.com>
2019-01-07 23:48:16 +0900

    Update token for Travis CI

--- assets/create.bash
+++ assets/create.bash
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+set -eo pipefail
+
 ASSETS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cargo run --release --bin refmt-generate-assets -- --assets-dir "$ASSETS_DIR"
